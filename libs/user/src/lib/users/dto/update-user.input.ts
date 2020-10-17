c3f7a4584ade3e85731b9685c2de79b9cb642c22
elboqueronpaco <elboqueronpaco@gmail.com>
2020-10-17 23:06:31 +0200

    creación input user

--- libs/user/src/lib/users/dto/update-user.input.ts
+++ libs/user/src/lib/users/dto/update-user.input.ts
@@ -4,6 +4,6 @@ import { InputType, Field, Int } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => String)
+  password: string;
 }
--- src/libs/user/src/lib/dto/update-user.input.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
-}
